903de269bd6fb72c7c7841f53d621a56356bb70b
Ryan Horn <rj.horn@gmail.com>
2023-03-04 10:28:43 -0700

    ryry changes

--- 02_REST/flatiron_theater_company/app/controllers/application_controller.rb
+++ 02_REST/flatiron_theater_company/app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+
+    def welcome
+        render json: {cat:"rose"}, status: :ok
+    end
+    
+    def name
+        render json:{greeting:"Hello #{params[:name]}"}, status: :ok
+    end
+
 end
--- 02_REST/flatiron_theater_company/config/routes.rb
+++ 02_REST/flatiron_theater_company/config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
-  resources :productions, only: [:index, :show, :create, :update, :destroy]
   
-
+  #exercise - buiid a custom GET route that goes to /welcome
+  # should point to application controller, and a method called welcome
+  get "/welcome", to: "application#welcome"
   
+  #build a dynamic route /greeting/<dynamic_name>
+  get "/greeting/:name", to: "application#name"
+  
+  resources :productions, only: [:index, :show]
+  
+  namespace :admin do
+    resources :productions, only: [:create, :update, :destroy]
+  end
 
 end
